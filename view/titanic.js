82cb9db068bd6ab0879a3afd4972858c72fc5ca0
Leojr2021 <leo.meza.mancilla@gmail.com>
2024-03-07 22:29:53 -0500

    view/add 3 charts for the frontend view

---
+++ view/titanic.js
@@ -0,0 +1,4 @@
+titanic = [ ['year','umich.edu','cam.ac.uk','indiana.edu','mac.com','berkeley.edu','uct.ac.za','unicon.net','ucdavis.edu','gmail.com','yale.edu'],
+['2005',57,7,12,12,12,14,6,11,10,5],
+['2006',753,342,317,315,252,248,220,194,171,141]
+];
